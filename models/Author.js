491ac1346f3d4a2014f9dc9fec3e67c0e750e2ec
Sebasan22 <callofsebas@hotmail.com>
2023-02-14 15:40:04 -0300

    author pass

--- models/Author.js
+++ models/Author.js
@@ -1,29 +1,33 @@
 module.exports = function (sequelize, Model, DataTypes) {
-    class Author extends Model {}
-Author.init(
-  {
-    id: {
-      type: DataTypes.INTEGER.UNSIGNED,
-      primaryKey: true,
-      autoIncrement: true,
+  class Author extends Model {}
+  Author.init(
+    {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      firstname: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      lastname: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(50),
+        allowNull: true,
+      },
+      password: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
     },
-    firstname: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-    },
-    lastname: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING(50),
-      allowNull: true,
-    },
-  },
-  {
-    sequelize,
-    modelName: "Author",
-  }
-);
-return Author;
-}
+    {
+      sequelize,
+      modelName: "Author",
+    }
+  );
+  return Author;
+};
--- seeders/authorSeeder.js
+++ seeders/authorSeeder.js
@@ -1,15 +1,15 @@
-const {faker} = require("@faker-js/faker")
+const { faker } = require("@faker-js/faker");
 
 module.exports = async function (Author) {
-  const authors = []
+  const authors = [];
   for (let i = 0; i < 40; i++) {
     authors.push({
       firstname: faker.name.firstName(),
       lastname: faker.name.lastName(),
       email: faker.internet.email(),
-     
+      password: faker.internet.password(),
     });
-  }  
+  }
 
   await Author.bulkCreate(authors);
 };
