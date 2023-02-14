a9c1f493c368826a666a035272c419b909a9c01e
KChino <kevintlapizaga@gmail.com>
2023-02-14 16:47:54 -0300

    Update commentController.js

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,12 +1,12 @@
 const { sequelize, Author, Article, Comment } = require("../models/index");
 
 const commentPost = async (req, res) => {
-    await Comment.create({
-      content: req.body.content,
-      AuthorId: req.body.author,
-      ArticleId: req.params.id,
-     })
-    res.redirect(`/articles/${req.params.id}`)
-    };
+  await Comment.create({
+    content: req.body.content,
+    AuthorId: req.body.author,
+    ArticleId: req.params.id,
+  });
+  res.redirect(`/articles/${req.params.id}`);
+};
 
-    module.exports = {commentPost}
+module.exports = { commentPost };
